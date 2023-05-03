640a2a8a9db13b93fcb991d3f8c44132bd45f921
zoevas <zoevas1@gmail.com>
2023-05-03 19:06:02 +0300

    font size

--- boxes.py
+++ boxes.py
@@ -5,11 +5,13 @@ import seaborn as sns
 
 color = ['#6495ED', '#B6D0E2']
 sns.set_palette(color)
+plt.rcParams['font.size'] = 14.0
 
 fields = ['Editor', 'Perceived Usefulness']
 df_perf = pd.read_csv('perceived_usefulness_it1_nonexperts.CSV', index_col=False)
 plt.rcParams["figure.figsize"] = (8, 4)
-plt.rcParams["xtick.labelsize"] = 7
+plt.rcParams["xtick.labelsize"] = 14
+
 sns_plot = sns.boxplot(y=df_perf["Perceived Usefulness"], x=df_perf["Editor"])
 sns_plot.set_xticklabels(sns_plot.get_xticklabels(), rotation=40, ha="right")
 plt.tight_layout()

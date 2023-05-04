a319e21a9859e609de90cae8decd95eb9b4dbbb0
zoevas <zoevas1@gmail.com>
2023-05-04 17:52:44 +0300

    sodalite case

--- boxes.py
+++ boxes.py
@@ -12,8 +12,8 @@ df_perf = pd.read_csv('perceived_usefulness_it1_nonexperts.CSV', index_col=False
 plt.rcParams["figure.figsize"] = (8, 4)
 plt.rcParams["xtick.labelsize"] = 14
 
-sns_plot = sns.boxplot(y=df_perf["Perceived Usefulness"], x=df_perf["Editor"])
-sns_plot.set_xticklabels(sns_plot.get_xticklabels(), rotation=40, ha="right")
+sns_plot = sns.boxplot(y=df_perf["Perceived Usefulness"], x=df_perf["Editor"], width=0.3)
+sns_plot.set_xticklabels(sns_plot.get_xticklabels(), rotation=15, ha="right")
 plt.tight_layout()
 fig = sns_plot.get_figure()
 fig.savefig("usefulness_non_experts.png", dpi=600)
---
+++ boxes_test/sodalite_boxes.py
@@ -0,0 +1,51 @@
+# Import libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+color = ['#6495ED', '#B6D0E2']
+sns.set_palette(color)
+plt.rcParams['font.size'] = 14.0
+
+fields = ['Editor', 'Perceived Usefulness']
+df_perf = pd.read_csv('perceived_usefulness_it1_nonexperts.CSV', index_col=False)
+plt.rcParams["figure.figsize"] = (8, 4)
+plt.rcParams["xtick.labelsize"] = 14
+
+sns_plot = sns.boxplot(y=df_perf["Perceived Usefulness"], x=df_perf["Editor"], width=0.3)
+sns_plot.set_xticklabels(sns_plot.get_xticklabels(), rotation=15, ha="right")
+plt.tight_layout()
+fig = sns_plot.get_figure()
+fig.savefig("usefulness_non_experts.png", dpi=600)
+
+df_perf = pd.read_csv('Perceived_Usefulness_experts_it1.csv', index_col=False)
+
+sns_plot = sns.boxplot(y=df_perf["Perceived Usefulness"], x=df_perf["Editor"], width=0.3)
+sns_plot.set_xticklabels(sns_plot.get_xticklabels(), rotation=15, ha="right")
+plt.tight_layout()
+fig = sns_plot.get_figure()
+fig.savefig("usefulness_experts.png", dpi=600)
+
+df_perf = pd.read_csv('Perceived_Usefulness_Use_Case_Owners_it1.csv', index_col=False)
+
+sns_plot = sns.boxplot(y=df_perf["Perceived Usefulness"], x=df_perf["Editor"], width=0.3)
+sns_plot.set_xticklabels(sns_plot.get_xticklabels(), rotation=15, ha="right")
+plt.tight_layout()
+fig = sns_plot.get_figure()
+fig.savefig("usefulness_use_case.png", dpi=600)
+
+df_perf = pd.read_csv('Ease_of_Use_Experts_it1.csv', index_col=False)
+
+sns_plot = sns.boxplot(y=df_perf["Perceived Ease of Use"], x=df_perf["Editor"], width=0.3)
+sns_plot.set_xticklabels(sns_plot.get_xticklabels(), rotation=15, ha="right")
+plt.tight_layout()
+fig = sns_plot.get_figure()
+fig.savefig("ease_of_use_experts.png", dpi=600)
+
+df_perf = pd.read_csv('Ease_of_Use_Use_Case_Owners_it1.csv', index_col=False)
+
+sns_plot = sns.boxplot(y=df_perf["Perceived Ease of Use"], x=df_perf["Editor"], width=0.3)
+sns_plot.set_xticklabels(sns_plot.get_xticklabels(), rotation=15, ha="right")
+plt.tight_layout()
+fig = sns_plot.get_figure()
+fig.savefig("ease_of_use_case_owners.png", dpi=600)

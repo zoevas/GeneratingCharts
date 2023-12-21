b3744204911d0cbb75d4bf5ea7726a5ad086b7cb
zoevas <zoevas1@gmail.com>
2023-12-21 13:19:50 +0200

    dpi increased

--- bar.py
+++ bar.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+dpi=800
+w=9
+h=6
 
 df = pd.read_csv("./Development_Modification.CSV")
-ax = df.plot.bar(x='Activity', color=['#336699', '#bfcfff'])
+ax = df.plot.bar(x='Activity', color=['#336699', '#bfcfff'], figsize=(w,h))
 ax.set_ylabel('Minutes', fontsize=16)
 
 activities = ['Development', 'Modification']
 ax.set_xticklabels(activities, rotation=0, fontsize=14)
 plt.tight_layout()
 plt.xlabel('Activity', fontsize=16);
-plt.savefig("timeExpertsIt1.png")
+plt.savefig("timeExpertsIt1.png", dpi=dpi)
